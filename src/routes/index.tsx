6aecb0f5646fa0c80332bb5dc10e00c05227bc06
gioboa <giorgiob.boa@gmail.com>
2023-07-14 08:53:09 +0200

    remove tailwind + add styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,26 +6,39 @@ import styles from './index.css?inline';
 export default component$(() => {
 	useStyles$(styles);
 	return (
-		<Tabs behavior='automatic'>
-			<h3>Danish Composers</h3>
-			<TabList>
-				<Tab>Maria Ahlefeldt</Tab>
-				<Tab>Carl Andersen</Tab>
-				<Tab>Ida Henriette da Fonseca</Tab>
-			</TabList>
-			<TabPanel>
-				<p>
-					Maria Theresia Ahlefeldt (16 January 1755 - 20 December 1810) was a
-					...
-				</p>
-			</TabPanel>
-			<TabPanel>
-				<p>Carl Joachim Andersen (29 April 1847 - 7 May 1909) was a ...</p>
-			</TabPanel>
-			<TabPanel>
-				<p>Ida Henriette da Fonseca (July 27, 1802 - July 6, 1858) was a ...</p>
-			</TabPanel>
-		</Tabs>
+		<>
+			<h1>Qwik UI Showcase</h1>
+			<Tabs behavior='automatic' class='tabs'>
+				<TabList>
+					<Tab class='tab-items'>Qwik</Tab>
+					<Tab class='tab-items'>Partytown</Tab>
+					<Tab class='tab-items'>Mitosis</Tab>
+				</TabList>
+				<TabPanel class='tab-panel'>
+					Qwik is a new kind of web framework that can deliver instant loading
+					web applications at any size or complexity. Your sites and apps can
+					boot with about 1kb of JS (regardless of application complexity), and
+					achieve consistent performance at scale.
+				</TabPanel>
+				<TabPanel class='tab-panel'>
+					Qwik is a new kind of web framework that can deliver instant loading
+					web applications at any size or complexity. Your sites and apps can
+					boot with about 1kb of JS (regardless of application complexity), and
+					achieve consistent performance at scale.
+				</TabPanel>
+				<TabPanel class='tab-panel'>
+					Partytown is a lazy-loaded library to help relocate resource intensive
+					scripts into a web worker, and off of the main thread. Its goal is to
+					help speed up sites by dedicating the main thread to your code, and
+					offloading third-party scripts to a web worker.
+				</TabPanel>
+				<TabPanel class='tab-panel'>
+					Mitosis is a subset of JSX. It supports generating code for a number
+					of frontend frameworks, including React, Vue, Angular, Svelte, and
+					Solid.
+				</TabPanel>
+			</Tabs>
+		</>
 	);
 });
 
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,26 +1,18 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
-
-import Header from '~/components/header/header';
-import Footer from '~/components/footer/footer';
-
 import styles from './styles.css?inline';
 
 export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
+	return {
+		date: new Date().toISOString(),
+	};
 });
 
 export default component$(() => {
-  useStyles$(styles);
-  return (
-    <>
-      <Header />
-      <main>
-        <Slot />
-      </main>
-      <Footer />
-    </>
-  );
+	useStyles$(styles);
+	return (
+		<main>
+			<Slot />
+		</main>
+	);
 });
